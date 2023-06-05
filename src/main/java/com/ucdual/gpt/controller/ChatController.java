f53b9b302b37e3f20a826250bd0af81ee82f1da1
LucasSimoesSilva <lucassimoes.carvalho@gmail.com>
2023-06-05 10:30:22 -0300

    Adicionando projeto no repositorio

---
+++ src/main/java/com/ucdual/gpt/controller/ChatController.java
@@ -0,0 +1,31 @@
+package com.ucdual.gpt.controller;
+
+import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.completion.CompletionChoice;
+import com.theokanning.openai.completion.CompletionRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+    @PostMapping
+    public ResponseEntity<List<CompletionChoice>> askToChat(@RequestBody String prompt) {
+        OpenAiService service = new OpenAiService("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");
+        CompletionRequest completionRequest = CompletionRequest.builder()
+                .prompt(prompt)
+                .model("text-davinci-003")
+                .maxTokens(100)
+                .build();
+
+        List<CompletionChoice> choices = service.createCompletion(completionRequest).getChoices();
+        return ResponseEntity.status(HttpStatus.OK).body(choices);
+    }
+}
---
+++ src/test/java/com/ucdual/gpt/GptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucdual.gpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
